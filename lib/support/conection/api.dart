be9e3b617caf1b43ff5621e60afc8243d848ecd5
Lucas Pedrazoli <lucaspedrazoli@gmail.com>
2019-06-01 16:25:55 -0300

    removes api

--- lib/support/conection/api.dart
+++ lib/support/conection/api.dart
@@ -2,17 +2,13 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-//Classe responsavel por realizar conexões com API
 class Api{
 
   final String urlBase;
 
   Api(this.urlBase);
 
-  /// Método que executa chamada de conexão do tipo GET
-  /// @params uri
-  /// @params headers (opcional)
-  Future <dynamic> get(String uri, {Map<String,String> headers}) async{
+  Future <dynamic> get(String uri, {Map<String,String> headers}) async {
 
     try{
 
@@ -33,81 +29,6 @@ class Api{
     }
 
   }
-
-  /// Método que executa chamada de conexão do tipo POST
-  /// @params uri
-  /// @params body
-  /// @params headers (opcional)
-  Future <dynamic> post(String uri,dynamic body, {Map<String,String> headers}) async{
-
-    try{
-
-      http.Response response = await http.post(urlBase+uri, body: body, headers:headers);
-
-      final statusCode = response.statusCode;
-
-      if(statusCode < 200 || statusCode >= 300) {
-        throw new FetchDataException("Error request:",statusCode);
-      }
-
-      const JsonDecoder decoder = const JsonDecoder();
-      return decoder.convert(response.body);
-
-    } on Exception catch(e){
-      throw new FetchDataException(e.toString(),0);
-    }
-
-  }
-
-  /// Método que executa chamada de conexão do tipo PUT
-  /// @params uri
-  /// @params body
-  /// @params headers (opcional)
-  Future <dynamic> put(String uri,dynamic body, {Map<String,String> headers}) async{
-
-    try{
-
-      http.Response response = await http.put(urlBase+uri, body: body, headers:headers);
-
-      final statusCode = response.statusCode;
-
-      if(statusCode < 200 || statusCode >= 300) {
-        throw new FetchDataException("Error request:",statusCode);
-      }
-
-      const JsonDecoder decoder = const JsonDecoder();
-      return decoder.convert(response.body);
-
-    } on Exception catch(e){
-      throw new FetchDataException(e.toString(),0);
-    }
-
-  }
-
-  /// Método que executa chamada de conexão do tipo DELETE
-  /// @params uri
-  /// @params headers (opcional)
-  Future <dynamic> delete(String uri, {Map<String,String> headers}) async{
-
-    try{
-
-      http.Response response = await http.delete(urlBase+uri, headers:headers);
-
-      final statusCode = response.statusCode;
-
-      if(statusCode < 200 || statusCode >= 300) {
-        throw new FetchDataException("Error request:",statusCode);
-      }
-
-      const JsonDecoder decoder = const JsonDecoder();
-      return decoder.convert(response.body);
-
-    } on Exception catch(e){
-      throw new FetchDataException(e.toString(),0);
-    }
-
-  }
-
 }
 
 class FetchDataException implements Exception {
