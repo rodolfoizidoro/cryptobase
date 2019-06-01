b8491a8edb1b1a7594cc5c12f7553e4045fd4aef
Lucas Pedrazoli <lucaspedrazoli@gmail.com>
2019-06-01 16:04:34 -0300

    remove code notice

--- lib/repository/notice_repository/notice_repository.dart
+++ lib/repository/notice_repository/notice_repository.dart
@@ -1,24 +1,13 @@
 
-import 'package:crypto_base/repository/notice_repository/model/notice.dart';
 import 'package:crypto_base/repository/notice_repository/model/highlight.dart';
 import 'package:crypto_base/support/conection/api.dart';
 
 abstract class NoticeRepository{
-  Future<List<Notice>> loadNews(String category, int page);
   Future<List<Highlight>> loadNewsRecent();
-  Future<List<Notice>> loadSearch(String query);
 }
 class NoticeRepositoryImpl implements NoticeRepository{
-
   final Api _api;
-
   NoticeRepositoryImpl(this._api);
-
-  Future<List<Notice>> loadNews(String category, int page) async {
-    final Map result = await _api.get("/notice/news/$category/$page");
-    return result['data']['news'].map<Notice>( (notice) => new Notice.fromMap(notice)).toList();
-  }
-
   Future<List<Highlight>> loadNewsRecent() async {
     final List result = await _api.get("https://cryptocontrol.io/api/v1/public/news?latest=true&key=8c697a23a2ce987425a5f54510df25d4");
 
@@ -29,17 +18,6 @@ class NoticeRepositoryImpl implements NoticeRepository{
     });
 
     return list;
-  }
-
-  Future<List<Notice>> loadSearch(String query) async {
-
-    final Map result = await _api.get("/notice/search/$query");
-
-    if(result['op']){
-      return result['data'].map<Notice>( (notice) => new Notice.fromMap(notice)).toList();
-    }else{
-      return List();
-    }
   }
 }
 
